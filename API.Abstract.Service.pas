3f037d8724eadeeb1bc17b2e01f9c8f02052075b
Ivonei Balena <iibalena@gmail.com>
2025-06-19 16:01:36 -0300

    Melhorado o método de envio para aceitar headers

--- API.Abstract.Service.pas
+++ API.Abstract.Service.pas
@@ -4,45 +4,59 @@ interface
 
 uses
   System.SysUtils, System.Classes, System.Net.HttpClient, System.JSON,
-  Service.JSONUtils;
+  Service.JSONUtils, System.Generics.Collections;
 
 type
   TAPIBaseService<TRequest: class; TResponse: class, constructor> = class
   public
-    class function Post(const AUrl: string; const AData: TRequest): TResponse;
+    class function Post(
+      const AUrl: string;
+      const AData: TRequest;
+      const AHeaders: TDictionary<string, string> = nil
+    ): TResponse;
   end;
 
 implementation
 
 { TAPIBaseService<TRequest, TResponse> }
 
-class function TAPIBaseService<TRequest, TResponse>.Post(const AUrl: string; const AData: TRequest): TResponse;
+class function TAPIBaseService<TRequest, TResponse>.Post(
+  const AUrl: string;
+  const AData: TRequest;
+  const AHeaders: TDictionary<string, string>
+): TResponse;
 var
   Client: THTTPClient;
   Response: IHTTPResponse;
   JsonRequest: string;
   JsonResponse: string;
-  RequestStream: TStringStream;
+  Stream: TStringStream;
+  Header: TPair<string, string>;
 begin
   Result := nil;
   Client := THTTPClient.Create;
-  RequestStream := nil;
   try
-    JsonRequest := TJSONUtils.ObjectToJsonString(AData);
-    RequestStream := TStringStream.Create(JsonRequest, TEncoding.UTF8);
-    Client.CustomHeaders['Content-Type'] := 'application/json';
+    JsonRequest := TJSONUtils.Serialize(AData);
+    Stream := TStringStream.Create(JsonRequest, TEncoding.UTF8);
+    try
+      if AHeaders <> nil then
+        for Header in AHeaders do
+          Client.CustomHeaders[Header.Key] := Header.Value;
 
-    Response := Client.Post(AUrl, RequestStream);
+      Response := Client.Post(AUrl, Stream);
 
-    if Response.StatusCode <> 200 then
-      raise Exception.CreateFmt('Erro ao enviar requisição: %s', [Response.StatusText]);
+      if Response.StatusCode <> 200 then
+        raise Exception.CreateFmt('Erro ao enviar requisição: %s', [Response.StatusText]);
 
-    JsonResponse := Response.ContentAsString(TEncoding.UTF8);
-    Result := TJSONUtils.Deserialize<TResponse>(JsonResponse);
+      JsonResponse := Response.ContentAsString(TEncoding.UTF8);
+      Result := TJSONUtils.Deserialize<TResponse>(JsonResponse);
+    finally
+      Stream.Free;
+    end;
   finally
-    RequestStream.Free;
     Client.Free;
   end;
 end;
 
 end.
+
