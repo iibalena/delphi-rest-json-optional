e887cf4f6c30b85abf6930df5d4944caff0a7bf1
Ivonei Balena <iibalena@gmail.com>
2025-06-19 15:17:15 -0300

    Versionamento dos arquivos do projeto

---
+++ DelphiRestJsonOptional.dpr
@@ -0,0 +1,22 @@
+program DelphiRestJsonOptional;
+
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+  Vcl.Forms,
+  Main.Demo in 'Main.Demo.pas' {MainDemo},
+  Model.Lancamento in 'Model.Lancamento.pas',
+  API.Types.Optional in 'API.Types.Optional.pas',
+  Service.JSONUtils in 'Service.JSONUtils.pas',
+  API.Abstract.Service in 'API.Abstract.Service.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Run;
+end.
