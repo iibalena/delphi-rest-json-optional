a81ecf7624e9c500134a12d422af47589538ec1b
Ivonei Balena <iibalena@gmail.com>
2025-06-19 21:02:49 -0300

    Adicionados novos arquivos ao projeto

--- DelphiRestJsonOptional.dpr
+++ DelphiRestJsonOptional.dpr
@@ -1,15 +1,12 @@
 program DelphiRestJsonOptional;
 
 uses
-  madExcept,
-  madLinkDisAsm,
-  madListHardware,
-  madListProcesses,
-  madListModules,
   Vcl.Forms,
   Main.Demo in 'Main.Demo.pas' {MainDemo},
   Model.Lancamento in 'Model.Lancamento.pas',
   API.Types.Optional in 'API.Types.Optional.pas',
+  API.LoggerHelper in 'API.LoggerHelper.pas',
+  API.Lancamento.Service in 'API.Lancamento.Service.pas',
   Service.JSONUtils in 'Service.JSONUtils.pas',
   API.Abstract.Service in 'API.Abstract.Service.pas';
 
