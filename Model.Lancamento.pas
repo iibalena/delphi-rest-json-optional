ed9df5f7582cfa5421cccccf15f5b64111d15347
Ivonei Balena <iibalena@gmail.com>
2025-06-20 09:44:56 -0300

    Alterado o model para incluir mais um campo opcional e usar no exemplo

--- Model.Lancamento.pas
+++ Model.Lancamento.pas
@@ -13,7 +13,7 @@ type
     FClassificacaoId: Integer;
     FNumeroDocumento: string;
     FDataVencimento: string;
-    FDataCompetencia: string;
+    FDataCompetencia: TOptional<string> ;
     FValor: Double;
     FPago: Boolean;
     FContaBancariaId: TOptional<Integer>;
@@ -24,7 +24,7 @@ type
     property ClassificacaoId: Integer read FClassificacaoId write FClassificacaoId;
     property NumeroDocumento: string read FNumeroDocumento write FNumeroDocumento;
     property DataVencimento: string read FDataVencimento write FDataVencimento;
-    property DataCompetencia: string read FDataCompetencia write FDataCompetencia;
+    property DataCompetencia: TOptional<string>  read FDataCompetencia write FDataCompetencia;
     property DataPagamento: TOptional<string> read FDataPagamento write FDataPagamento;
     property ContaBancariaId: TOptional<Integer> read FContaBancariaId write FContaBancariaId;
     property Valor: Double read FValor write FValor;
